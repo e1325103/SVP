a6b4359f3abd9145ef58bec6f327d82e75abf1fa
1327052 <e1327052@student.tuwien.ac.at>
2017-05-20 19:13:44 +0200

    Streamline and Pathline Visualization, GUI

---
+++ SVP/SVP/SVP/RungeKutta.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVP
+{
+    public class RungeKutta
+    {
+        private int numPoints;
+        private int steps;
+        private float delta;
+
+        private Vec2 rectanglePos; // Rectangular Area to generate Seedpoints
+        private Vec2 rectanglePos2;
+        private VectorField field;
+
+        private Random random;
+
+        public ArrayList lines;
+
+        private float timePerStep;
+
+        public RungeKutta(int numPoints, int steps, float delta, Vec2 rectanglePos, Vec2 rectanglePos2, VectorField field)
+        {
+            this.numPoints = numPoints;
+            this.steps = steps;
+            this.delta = delta;
+
+            this.rectanglePos = rectanglePos;
+            this.rectanglePos2 = rectanglePos2;
+            this.field = field;
+
+            this.lines = new ArrayList();
+
+            this.random = new Random();
+        }
+
+        public RungeKutta(int numSeeds, int steps, float delta, Vec2 rectanglePos, Vec2 rectanglePos2, VectorField field, float time)
+        {
+            this.numPoints = numSeeds;
+            this.steps = steps;
+            this.delta = delta;
+
+            this.rectanglePos = rectanglePos;
+            this.rectanglePos2 = rectanglePos2;
+            this.field = field;
+
+            this.lines = new ArrayList();
+
+            this.random = new Random();
+
+            this.timePerStep = time;
+        }
+
+        public void generate()
+        {
+            bool outside = false;
+
+            //float stepSize = 
+              
+            for (int i = 0; i < numPoints; i++)
+            {
+                Vec2 seed = generateSeedPoint();
+                Line line = new Line();
+
+                float x = seed.X;
+                float y = seed.Y;
+
+                if (isOutside(x, y))
+                {
+                    outside = true;
+                }
+
+                for (int j = 0; j < steps && !outside; j++)
+                {
+                    Vec2 lookAt = field.interpolateTrilinear(x, y, 0);
+
+                    if (lookAt.X != 0 && lookAt.Y != 0)
+                    {
+                        lookAt = lookAt.normalize();
+
+                        lookAt.X = x + lookAt.X * delta / 2;
+                        lookAt.Y = y + lookAt.Y * delta / 2;
+
+                        if (!isOutside(lookAt.X, lookAt.Y))
+                        {
+                            Vec2 actualDir = field.interpolateTrilinear(lookAt.X, lookAt.Y, 0);
+
+                            if (actualDir.X != 0 && actualDir.Y != 0)
+                            {
+                                actualDir = actualDir.normalize();
+
+                                x = x + actualDir.X * delta;
+                                y = y + actualDir.Y * delta;
+
+                                if (!isOutside(x, y))
+                                {
+                                    line.add(new Vec2(x, y));
+                                }
+                                else
+                                {
+                                    outside = true;
+                                }
+                            }
+                            else
+                            {
+                                outside = true;
+                            }
+                        }
+                        else
+                        {
+                            outside = true;
+                        }
+                    }
+                    else
+                    {
+                        outside = true;
+                    }
+                }
+
+                outside = false;
+
+                lines.Add(line);
+            }
+        }
+
+        private bool isOutside(float x, float y)
+        {
+            if (x < 0 || y < 0)
+            {
+                return true;
+            }
+
+            if (x >= (field.size - 2) || y >= (field.size - 2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Vec2 generateSeedPoint()
+        {            
+            return new Vec2(
+                random.Next(((int)rectanglePos.X), ((int)rectanglePos2.X)),
+                random.Next(((int)rectanglePos.Y), ((int)rectanglePos2.Y))
+            );
+        }
+
+        public  void generatePathLines()
+        {
+            bool outside = false;
+            float currentTime = 0;
+
+            //float stepSize = 
+
+            for (int i = 0; i < numPoints; i++)
+            {
+                Vec2 seed = generateSeedPoint();
+                Line line = new Line();
+
+                currentTime = 0;
+
+                float x = seed.X;
+                float y = seed.Y;
+
+                if (isOutside(x, y))
+                {
+                    outside = true;
+                }
+
+                for (int j = 0; j < steps && !outside; j++)
+                {
+                    Vec2 lookAt = field.interpolateTrilinear(x, y, currentTime);
+
+                    if (lookAt.X != 0 && lookAt.Y != 0)
+                    {
+                        lookAt = lookAt.normalize();
+
+                        lookAt.X = x + lookAt.X * delta / 2;
+                        lookAt.Y = y + lookAt.Y * delta / 2;
+
+                        if (!isOutside(lookAt.X, lookAt.Y))
+                        {
+                            Vec2 actualDir = field.interpolateTrilinear(lookAt.X, lookAt.Y, currentTime);
+
+                            if (actualDir.X != 0 && actualDir.Y != 0)
+                            {
+                                actualDir = actualDir.normalize();
+
+                                x = x + actualDir.X * delta;
+                                y = y + actualDir.Y * delta;
+
+                                currentTime += timePerStep;
+
+                                if (currentTime > 47)
+                                {
+                                    currentTime = 47;
+                                }
+
+                                if (!isOutside(x, y))
+                                {
+                                    line.add(new Vec2(x, y));
+                                }
+                                else
+                                {
+                                    outside = true;
+                                }
+                            }
+                            else
+                            {
+                                outside = true;
+                            }
+                        }
+                        else
+                        {
+                            outside = true;
+                        }
+                    }
+                    else
+                    {
+                        outside = true;
+                    }
+                }
+
+                outside = false;
+
+                lines.Add(line);
+            }
+        }
+    }
+}
---
+++ SVP/SVP/SVP/Vec2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVP
+{
+    public class Vec2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vec2(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static Vec2 operator *(Vec2 v, float scalar)
+        {
+            return new Vec2(v.X * scalar, v.Y * scalar);
+        }
+
+        public static Vec2 operator +(Vec2 v1, Vec2 v2)
+        {
+            return new Vec2(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vec2 operator /(Vec2 v, float scalar)
+        {
+            return new Vec2(v.X / scalar, v.Y / scalar);
+        }
+
+        public Vec2 normalize()
+        {
+            float rootsum = ((float)Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2)));
+            return this / rootsum;
+        }
+
+        public Vec2 dot(Vec2 other)
+        {
+            return new Vec2(this.X * other.X, this.Y * other.Y);
+        }
+    }
+}
