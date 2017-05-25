f9ff44e6f3e971c940c291fc76e497fdfbea5d1a
1325103 <e1325103@student.tuwien.ac.at>
2017-05-25 21:50:19 +0200

    Changed travel simulation

--- Matlab/simulateTravels.m
+++ Matlab/simulateTravels.m
@@ -1,14 +1,14 @@
 close all;
 shr = 1000000;
 wig = 3000000;
-load coastlines
+%load coastlines;
 
 countries = readtable('countries.csv', 'Delimiter', ';');
 travel = readtable('travelAT.csv', 'Delimiter', ';');
-scatter(table2array(countries(:, 3)) / shr, table2array(countries(:, 2)) / shr);
-hold on;
-plot(coastlon, coastlat);
-hold off;
+%scatter(table2array(countries(:, 3)) / shr, table2array(countries(:, 2)) / shr);
+%hold on;
+%plot(coastlon, coastlat);
+%hold off;
 count = 1;
 for v = travel.ID'
    if ~ismember(v, countries.ID)
@@ -19,9 +19,9 @@ for v = travel.ID'
 end
 j = join(travel, countries, 'Keys', 'ID');
 
-figure;
-hold on;
-plot(coastlon, coastlat);
+%figure;
+%hold on;
+%plot(coastlon, coastlat);
 atpos(1) = countries(13, 3).LON;
 atpos(2) = countries(13, 2).LAT;
 connections = [];
@@ -30,10 +30,8 @@ for e = 1:size(j, 1)
         x = [(atpos(1) + rand(1) * wig) / shr, (j(e, 4).LON + rand(1) * wig) / shr];
         y = [(atpos(2) + rand(1) * wig) / shr, (j(e, 3).LAT + rand(1) * wig) / shr];
         connections = [connections; x, y];
-        plot(x, y);
     end
-    j(e, 5).NAME
 end
-hold off;
-xlim([-60 60])
-ylim([-20 80])
+% hold off;
+% xlim([-40 60])
+% ylim([30 80])
